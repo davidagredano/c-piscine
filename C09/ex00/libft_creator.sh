c08ef2cf39a24d00abec2f1f05008e3faffa3c40
David Agredano <davidagredano@gmail.com>
2024-11-28 21:27:16 +0100

    Update and document C09 ex00

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Compile all .c files into object files
 cc -c *.c
-ar rc libft.a *.o
+
+# Create a static library from the object files
+#  r (replace): Add or replace object files in the archive 
+#  c (create): Create the archive if it doesn't exist
+#  s (index): Generates an index (symbol table) for faster linking
+ar rcs libft.a *.o
+
+# Remove all object files
 rm *.o
-ranlib libft.a
